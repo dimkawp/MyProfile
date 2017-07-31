a5abb74b1c4f33974347f3199582aff4d01f9115
dimkawp <dimkawp@gmail.com>
2017-07-31 17:50:57 +0300

    Work on admin environment.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by email: params[:user][:email]
+    @articles = Article.all
+    @works = Work.all
+    if user
+      if user.email == params[:user][:email] && user.password == params[:user][:password]
+        session[:user_id] = user.id
+        render 'admin/index'
+      end
+    else
+      redirect_to '/login', :flash => { :error => 'Bad email or password.' }
+
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    render 'new'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+
+  get 'sessions/new'
+
   root 'home#index'
   get '/' 'home/index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
+  get '/admin', to: 'admin#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
